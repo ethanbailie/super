064e887e4e8190c92044bae4f6d08870dd3f4aad
Ethan Bailie <ethanbailie@hotmail.com>
2024-12-06 13:02:43 -0500

    fixed up customer lifetime value
    made it so you can see a running total monthly

--- super/models/customer_lifetime_value.sql
+++ super/models/customer_lifetime_value.sql
@@ -7,16 +7,22 @@
 with customer_value as (
     select 
         customer_id,
+        date_trunc('month', created_at) as order_month,
         sum(total_price) as customer_total
     from {{ source('raw', 'orders') }}
     where refunded_at is null
-    group by customer_id
+    group by all
 )
 
 select
     cohorts.cohort_month,
-    trunc(sum(customer_value.customer_total) / count(cohorts.customer_id), 2) as cohort_customer_lifetime_value
+    customer_value.order_month,
+    trunc(sum(customer_value.customer_total) / count(cohorts.customer_id), 2) as monthly_value,
+    sum(monthly_value) over (
+        partition by cohort_month
+        order by order_month
+    ) as cohort_customer_lifetime_value
 from {{ ref('cohorts') }} as cohorts
-left join customer_value
+inner join customer_value
     on cohorts.customer_id = customer_value.customer_id
-group by cohorts.cohort_month
+group by all
